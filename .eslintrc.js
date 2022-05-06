de67f17b17666769199aaecffa018de7d2a75ebe
SnowBallGhost <47889538+MaybePixem@users.noreply.github.com>
2022-05-06 15:53:18 +0200

    configure husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
 	env: {
 		browser: true,
-		es2021: true
+		es2021: true,
+		node: true
 	},
 	extends: [
-		'plugin:react/recommended',
-		'eslint:recommended'
+		"plugin:react/recommended",
+		"eslint:recommended"
 	],
-	parser: '@typescript-eslint/parser',
+	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true
 		},
-		ecmaVersion: 'latest',
-		sourceType: 'module'
+		ecmaVersion: "latest",
+		sourceType: "module"
 	},
 	plugins: [
-		'react',
-		'@typescript-eslint'
+		"react",
+		"@typescript-eslint"
 	],
 	rules: {
 		"arrow-spacing": "error",
@@ -30,5 +31,6 @@ module.exports = {
 		"no-unused-vars": "error",
 		"quotes": ["warn", "double", { "avoidEscape": true }],
 		"semi": ["warn", "always"],
+		"react/react-in-jsx-scope": "off"
 	}
-}
+};
