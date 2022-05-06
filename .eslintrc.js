dd13d5fcaf1a20e94838e39d5e9d23506c2bab77
SnowBallGhost <47889538+MaybePixem@users.noreply.github.com>
2022-05-06 15:26:08 +0200

    configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,34 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'plugin:react/recommended',
+		'eslint:recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: [
+		'react',
+		'@typescript-eslint'
+	],
+	rules: {
+		"arrow-spacing": "error",
+		"comma-dangle": ["warn", "only-multiline"],
+		"no-alert": "error",
+		"no-console": "warn",
+		"no-multi-spaces": "warn",
+		"no-undef": "error",
+		"no-unreachable": "warn",
+		"no-unused-vars": "error",
+		"quotes": ["warn", "double", { "avoidEscape": true }],
+		"semi": ["warn", "always"],
+	}
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 	root
 );
 
